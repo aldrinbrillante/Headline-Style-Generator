4aa9ace8b3200b624d1bb757c1608071dd748624
Aldrin Brillante <aldrinbrillante@Aldrins-MacBook-Pro.local>
2020-10-06 16:00:04 -0700

    const commits

---
+++ script.js
@@ -0,0 +1,4 @@
+// The headline element
+const display = document.querySelector('#display')
+const inputBGColor = document.querySelector('#input-bg-color')
+const inputColor = document.querySelector('#input-color')
